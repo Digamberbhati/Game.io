590b437960bb1c835b0eb001407b920a8fd78714
Digamber Bhati <93906020+Digamberbhati@users.noreply.github.com>
2024-07-23 15:57:11 +0530

    c++
    demo

---
+++ sandbox.h
@@ -0,0 +1,9 @@
+#ifndef SANDBOX_H
+#define SANDBOX_H
+
+#include "setup.h"
+
+void sandbox_init(GameObjects *go, Textures *t);
+void sandbox_update(GameObjects *go, Textures *t, GameControls *gc);
+
+#endif
